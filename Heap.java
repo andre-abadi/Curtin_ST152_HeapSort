1df8539fbb0b832eda97bdf2151e3a4cb065ec5f
sajuuk <eagle@.(none)>
2011-05-25 11:01:26 +0800

    Debugging addition to empty array.

--- Heap.java
+++ Heap.java
@@ -7,9 +7,9 @@ package heapsort;
 public class Heap {
 
   //stores the heap in array form
-  private HeapEntry[] heap;
+  public HeapEntry[] heap;
   //keeps a count of the current, last array index used
-  private int latestIndex;
+  public int latestIndex;
 
 
   /**
@@ -31,6 +31,9 @@ public class Heap {
       //add the given object to the new last-index of the array
       heap[latestIndex] = new HeapEntry(pty, vlue);
       //increment the latestIndex pointer int
+      if (latestIndex) {
+        
+      }
       latestIndex += 1;
     } catch (Exception e) {
       //inform the user of errors adding to array
@@ -45,11 +48,16 @@ public class Heap {
   public void trickleUp() {
     //start by setting a counter at the back of the occupied cells
     int current = latestIndex;
+    System.out.println("Current node initialised to " + current);
     //go up as long as our current position doesn't go beyond index 0
-    while (current >= 0) {
+    while (current > 0) {
+      System.out.println("Current node changed to " + current);
       //calculate the index of the parent of our node
       int parent = (current-1)/2;
+      System.out.println("Parent node changed to " + parent);
       //if the added entry has higher priority than parent, then swap them
+      System.out.println("Current priority " + heap[current].priority);
+      System.out.println("Parent priority " + heap[parent].priority);
       if (heap[current].priority > heap[parent].priority) {
         swapEntries(heap, current, parent);
       }
@@ -64,7 +72,8 @@ public class Heap {
    * @param indexA int The index of the first entry to be swapped
    * @param indexB int The index of the second entry to be swapped
    */
-  private void swapEntries(HeapEntry[] heap, int indexA, int indexB) {
+  public void swapEntries(HeapEntry[] heap, int indexA, int indexB) {
+    System.out.println("Swapping entries at "+indexA+" and "+indexB);
     HeapEntry tempEntry = new HeapEntry(heap[indexB]);
     heap[indexB] = new HeapEntry(heap[indexA]);
     heap[indexA] = new HeapEntry(tempEntry);
@@ -89,34 +98,57 @@ public class Heap {
       top = new HeapEntry(heap[0]);
       //swap the end of the array with the start of the array
       swapEntries(heap, 0, latestIndex);
+      //decrement the latest entry index since we have now
+      //moved an entry from the last place to the first
+      latestIndex -= 1;
     }
     return top;
   }
 
+  /**
+   * Takes the first array element and trickles it to its rightful place.
+   */
   public void trickleDown() {
     int current = 0;
+    int leftChild = 1;
+    int rightChild = 2;
+    int largerChild = leftChild;
     //keep going as long as there is a left child
     //i.e. keep going as long as there is another level of children below
-    while (((current*2)+1) <= latestIndex) {
+    while (leftChild <= latestIndex) {
         //compute the index of the left child
-        int leftChild = ((current*2)+1);
+        leftChild = ((current*2)+1);
         //compute the index of the right child
-        int rightChild = ((current*2)+2);
+        rightChild = ((current*2)+2);
         //default the larger child to the left child
-        int largerChild = leftChild;
+        largerChild = leftChild;
         //if there is no right child, we don't need to select the larger child
-        if (heap[rightChild] == null) {
+        if (rightChild > latestIndex) {
           //and can just compare the left
           if (heap[largerChild].priority > heap[current].priority) {
+            //and swap the entries if the left (larger) child
+            //has higher priority
             swapEntries(heap,largerChild,current);
           }
-
         } else {
+          //otherwise, if there exists a right child
+          //calculate which of the children has higher priority
           if (heap[rightChild].priority > heap[leftChild].priority) {
+            //if the right child has higher priority, set it as
+            //the larger child
             largerChild = rightChild;
+            //if it isn't we needn't do anything since larger child
+            //defaults to the left child
           }
-          swapEntries(heap,largerChild,current);
+          //then compare the larger child with the current node
+          if (heap[largerChild].priority > heap[current].priority) {
+            //and swap them if that child has a higher priority
+            swapEntries(heap,largerChild,current);
+          }
         }
+        //then we should increment current to the larger child
+        //and head back to the top of our while loop to see if we've
+        //hit the end yet
         current = largerChild;
     }
   }
