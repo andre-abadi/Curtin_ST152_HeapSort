9e6d6af8d3907f7bf4da7cb72a184a71d84890e6
sajuuk <eagle@.(none)>
2011-05-25 19:17:21 +0800

    Cleaned up add and remove.

--- Heap.java
+++ Heap.java
@@ -7,9 +7,9 @@ package heapsort;
 public class Heap {
 
   //stores the heap in array form
-  public HeapEntry[] heap;
+  private HeapEntry[] heap;
   //keeps a count of the current, last array index used
-  public int latestIndex;
+  private int latestIndex;
 
 
   /**
@@ -23,23 +23,20 @@ public class Heap {
 
   /**
    * Adds an entry to the heap, sorting the heap for priority afterwards.
-   * @param pty The priority of the entry to be added
-   * @param vlue The Object to be added in the entry, to the heap
+   * @param entry HeapEntry the entry to be added
    */
-  public void add(int pty, Object vlue) {
-    try {
-      //add the given object to the new last-index of the array
-      heap[latestIndex] = new HeapEntry(pty, vlue);
-      //increment the latestIndex pointer int
-      if (latestIndex) {
-        
-      }
+  public void add(HeapEntry entry) {
+    //only add to the array if there is room to add
+    if (latestIndex < heap.length) {
+      //start by incrementing the latestIndex counter
       latestIndex += 1;
-    } catch (Exception e) {
-      //inform the user of errors adding to array
-      System.out.println("Out of space in heap: " + e.getMessage());
+      //then copy in the new object to this new, last place
+      heap[latestIndex] = new HeapEntry(entry);
+      //then trickle the new value until it reaches the right spot
+      //trickleUp();
+    } else {
+      System.out.println("Could not insert, heap is full.");
     }
-    trickleUp();
   }
 
   /**
@@ -73,7 +70,6 @@ public class Heap {
    * @param indexB int The index of the second entry to be swapped
    */
   public void swapEntries(HeapEntry[] heap, int indexA, int indexB) {
-    System.out.println("Swapping entries at "+indexA+" and "+indexB);
     HeapEntry tempEntry = new HeapEntry(heap[indexB]);
     heap[indexB] = new HeapEntry(heap[indexA]);
     heap[indexA] = new HeapEntry(tempEntry);
@@ -84,25 +80,21 @@ public class Heap {
    * @return Object The object at the top of the heap (highest priority)
    */
   public HeapEntry remove() {
-    //initialise a variable to store the top of the heap that we will return
-    HeapEntry top = null;
-    //do the following only if the heap has its counter past 0
-    if (heap.length == 0) {
-      //otherwise inform the user that the heap is empty
-      System.out.println("Heap is empty, try adding some values first.");
-    } else {
-      //store a variable to keep track of where we are in the tree
-      //starting at the root of the tree
-      int current = 0;
-      //part 1; pop the highest priority; the top of the heap
-      top = new HeapEntry(heap[0]);
-      //swap the end of the array with the start of the array
-      swapEntries(heap, 0, latestIndex);
-      //decrement the latest entry index since we have now
-      //moved an entry from the last place to the first
+    if (latestIndex<0) {
+      //first pop off the top of the heap and assign to a return variable
+      HeapEntry top = new HeapEntry(heap[0]);
+      //then move the last entry to the first, so the tree is almost full
+      swapEntries(heap,0,latestIndex);
+      //then decrement the latestIndex counter so that we ignore the
+      //now 'empty' last entry
       latestIndex -= 1;
+      //lastly, trickle the new top of the heap down to its rightful place
+      //trickleDown();
+      return top;
+    } else {
+      System.out.println("Could not remove, heap is empty.");
+      return null;
     }
-    return top;
   }
 
   /**
